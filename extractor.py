668093c7dc812fbd65fa4fbc23b0c6f1fc0a639d
Margaret <mqwang@mit.edu>
2024-12-08 03:17:38 -0500

    hehe haha need to clean this up

--- extractor.py
+++ extractor.py
@@ -6,7 +6,7 @@ with open("schema.txt", "r") as f:
     image_id = "61283218_90"
     request_text = f"""Given this schema {schema}, extract each field of the schema from the image
     and return the results in a JSON format. Return only the JSON. For the ID field, set it to {image_id}.
-    Also add an estimate of how confident you are in your answers for each field extracted."""
+   """
     image_path = f"images/{image_id}.jpg"
     encoded_image = encode_image_to_base64(image_path)
     response = send_request_with_image(request_text, encoded_image)
--- schema.py
+++ schema.py
@@ -3,9 +3,9 @@ from utils import send_request_text_only, response_to_content
 request_text = """ Given the following list of queries, propose a schema for a dataset of images of roads.
 Each image will have a unique ID and we do not need to store the image data.
 Format the response so that only the schema is provided and do so in a text-based format.
-What is the proportion of protected to unprotected bike lanes?
-Are there any obstructions in the bike lane (e.g. illegally parked cars, trash bins, etc.)?
-Are these bike lanes biker-only or shared with pedestrians? Buses?
+Are these bike lanes protected by a barrier, protected by separation, or unprotected?
+Are these bikes lanes obstruction-free, obstructed by cars, obstructed by trash bins, or obstructed by something else?
+Are these bike lanes biker-only, shared with pedestrians, or shared with buses?
 """
 response = send_request_text_only(request_text)
 content = response_to_content(response)
